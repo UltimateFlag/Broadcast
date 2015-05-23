951d6bd0643f347c9269e138c7359228f2953e13
Paul Hollinsky <paulhollinsky@gmail.com>
2015-05-23 19:05:05 -0400

    New testing framework

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var socket = require('socket.io-client')('http://hollinsky.com:6969');
+var socket = require('socket.io-client')('http://localhost:6969'); //('http://hollinsky.com:6969');
 
 socket.on('connect', function(){
 	socket.emit('setName', "Paul");
